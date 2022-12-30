082f63fe94647bafb2703e04c6b0e53c325abfb5
gaodesergiu <gaodes@gmail.com>
2022-12-30 19:09:01 +0200

    updated location coordnates

--- config.js
+++ config.js
@@ -32,8 +32,8 @@ const CONFIG = {
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
 	trackLocation: true, // If false or an error occurs, the app will use the lat/lon below
-	defaultLatitude: '37.775',
-	defaultLongitude: '-122.419',
+	defaultLatitude: '45.3050',
+	defaultLongitude: '21.8821',
 
 	// Autochange
 	autoChangeTheme: true,
